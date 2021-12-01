9d153e17b5317e1ad47ec2364c3e0c516793aca8
eniola <eniolaakinpelu6@gmail.com>
2021-12-02 00:37:54 +0100

    second commit

---
+++ src/ArticlesLists.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ArticlesLists = ({ article, title }) => {
+  return (
+    <>
+      <h1>{title}</h1>
+      {article.map((article) => (
+        <div className="article-preview" key={article.id}>
+          <h2>{article.title}</h2>
+          <p>Written by {article.author}</p>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default ArticlesLists;
---
+++ src/links/About.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Home from "./Home";
+
+const About = () => {
+  return <div></div>;
+};
+
+export default About;
